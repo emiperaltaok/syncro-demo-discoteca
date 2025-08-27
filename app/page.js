192c8ee3b71c51bb809c1f3eb7e82a861a4e3617
emiperaltaok <emiperaltaok@gmail.com>
2025-08-27 19:07:36 -0300

    titulo syncro analititca

--- app/page.js
+++ app/page.js
@@ -210,7 +210,7 @@ export default function HomePage() {
 
       <div className="flex justify-between items-center mb-8 flex-wrap gap-5">
         <h1 className="text-4xl font-bold bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent">
-          SYNCRO Analytics Data PRO
+          SYNCRO Analitica
         </h1>
         <div className="flex items-center gap-4">
           <div className={`px-4 py-2 rounded-full backdrop-blur-lg border ${
